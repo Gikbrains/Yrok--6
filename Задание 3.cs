5f9d4671fe3e15fa38dcb183fbc1ccc41ba593e9
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-03-08 23:06:55 +0300

    Add files via upload

---
+++ Задание 3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int(String[][] arr)  MyArraySizeException
+                {
+                int count = 0;
+                if = 4;
+            }
+            {
+                throw new MyArraySizeException();
+            }
+            for (int i = 0; i < arr.length; i++)
+            {
+                if (arr[i].length! = 4)
+            }
+            {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++)
+            {
+                count = count + Integer.parseInt(arr[i][j]);
+            }
+            catch (NumberFormatException e)
+            {
+                MyArrayDataException(i, j);
+            }
+        }
+    }
+}
